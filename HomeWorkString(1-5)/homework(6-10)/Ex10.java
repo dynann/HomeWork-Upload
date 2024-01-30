f87a429f82aa6a05b7a40ef7edccfe2bf0078959
Potato <137735702+rthjr@users.noreply.github.com>
2024-01-30 18:25:07 +0700

    Update and rename Ex5.java to Ex10.java

--- HomeWorkString(1-5)/homework(6-10)/Ex10.java
+++ HomeWorkString(1-5)/homework(6-10)/Ex10.java
@@ -2,7 +2,7 @@ package week4;
 
 import java.util.Scanner;
 
-public class Ex5 {
+public class Ex10 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
