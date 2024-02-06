8b070d933762d60e4828417f5883daa6cffcd6ff
dynann <149320775+dynann@users.noreply.github.com>
2024-02-06 21:53:43 +0700

    Update Exercise2.java

--- Yang Sokkheang/Exercise2.java
+++ Yang Sokkheang/Exercise2.java
@@ -1,32 +1,26 @@
-    import java.util.HashSet;
-    import java.util.Set;
-        public class Exercise2 {
-        
-     }
-        public static void main(String[] args) {
-            String[] array1 = {"apple", "banana", "orange", "kiwi", "banana"};
-            String[] array2 = {"banana", "kiwi", "grape", "apple"};
-    
-            Set<String> set1 = new HashSet<>();
-            Set<String> intersection = new HashSet<>();
-    
-            // Add elements of array1 to set1
-            for (String s : array1) {
-                set1.add(s);
-            }
-    
-            // Check for common elements between array2 and set1
-            for (String s : array2) {
-                if (set1.contains(s)) {
-                    intersection.add(s);
-                }
-            }
-    
-            // Display non-duplicated intersections
-            System.out.println("Intersection of array1 and array2:");
-            for (String s : intersection) {
-                System.out.println(s);
-            }
-        }
-    }    
+//find intersection of two array of string
+public class Excerise2 {
+public static void main(String[] args) {
+    String[] array1 = {"apple", "banana", "orange", "kiwi", "banana"};
+    String[] array2 = {"banana", "kiwi", "grape", "apple"};
+
+    int interSize = Math.min(array1.length , array2.length);
+
+    String[] intersection = new String[interSize];
+
+    // Check for common elements between array2 and set1
+      for( int i = 0 ; i < array1.length ; i++){
+          for ( int j = 0 ; j < array2.length ; j++){
+              if(array1[i] == array2[j]){
+                  intersection[j] = array1[i];
+              }
+          }
+      }
+
+    // Display non-duplicated intersections
+    System.out.println("Intersection of array1 and array2:");
+    for (String s : intersection) {
+        System.out.print(s + " ");
+    }
+   }
 }
