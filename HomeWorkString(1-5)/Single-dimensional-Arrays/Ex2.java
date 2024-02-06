53d8cae86e5846caf48396e93f763e69acd4fc27
dynann <149320775+dynann@users.noreply.github.com>
2024-02-06 21:54:47 +0700

    Delete HomeWorkString(1-5)/Single-dimensional-Arrays/Ex2.java

--- HomeWorkString(1-5)/Single-dimensional-Arrays/Ex2.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Ex2 {
-    public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        int intersectionSize = 0 ;
-
-    }
-}
