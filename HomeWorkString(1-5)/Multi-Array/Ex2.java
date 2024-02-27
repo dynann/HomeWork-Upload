ef99890d4c4fd41a1742b4fb95d28afa12bad7d5
Rithjr <137735702+rthjr@users.noreply.github.com>
2024-02-27 18:09:11 +0700

    Add files via upload

---
+++ HomeWorkString(1-5)/Multi-Array/Ex2.java
@@ -0,0 +1,36 @@
+public class Ex2 {
+ public static void main(String[] args) {
+    // todo set variable and multi array
+    int[][] multiArray = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9},
+    };
+    int length = multiArray.length;
+
+    // todo print all the diagonal
+    for(int i = 0; i < length + 2; i++){
+
+        // todo get the small row
+        int startRow = Math.min(i, length - 1);
+
+        // todo get the hight col
+        int startCol = Math.max(0, i - length + 1);
+
+        // todo set value to rwo and col
+        int row = startRow;
+        int col = startCol;
+
+        // todo access the array
+        while(row >= 0 && col < length){
+            
+            System.out.print(multiArray[row][col] + " ");
+            
+            // todo to rotate row and col
+            row--;
+            col++;
+        }
+        System.out.println();
+    }
+ }   
+}
